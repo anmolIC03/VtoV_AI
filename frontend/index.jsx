7dfb4700ec5288604aa67e38116c03a09d6b9ed7
anmolIC03 <146379043+anmolIC03@users.noreply.github.com>
2026-03-03 12:52:25 +0530

    Fix import statement for GeminiAdventureApp

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GeminiAdventureApp from './app.jsx' // Make sure the import matches your app.jsx export name
+import GeminiAdventureApp from './app.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
